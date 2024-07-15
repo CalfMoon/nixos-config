c2b24576c1d1010ec84a40c1e1db1ceefbef52da
Kritagya Bhattarai (CalfMoon) <kritagyabhattarai@proton.me>
2024-07-15 16:13:50 +0545

    add new kernel param

--- system-config/configuration.nix
+++ system-config/configuration.nix
@@ -8,7 +8,7 @@
   boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
   boot.kernelModules = [ "v4l2loopback" "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
   boot.extraModprobeConfig = ''options v4l2loopback video_nr=0 exclusive_caps=1 card_label="Virtual Camera"'';
-  boot.kernelParams = [ "nvidia_drm.modeset=1" "nvidia_drm.fbdev=1" ];
+  boot.kernelParams = [ "nvidia_drm.modeset=1" "nvidia_drm.fbdev=1" "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
 
   nix.settings.allowed-users = [ "mooney" ];
 
